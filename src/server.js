ad45f9758c7fae1d53577958667ae10bc9853f24
Heberto <hebertourribarri2@gmail.com>
2021-02-05 12:43:12 -0400

    Casco de seguridad añadido

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const helmet = require("helmet");
 
 //inicializaciones
 const app = express();
 
 //Middlewares
 app.use(cors());
+app.use(helmet());
 // Analizar la solicitud
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // ruta principal
 //https://api-rest-s.herokuapp.com
 app.get("/", (req, res) => {
-  res.json({ message: "Hola mundo."});
+  res.json({ message: "Hola mundo :)"});
 });
 
 //rutas

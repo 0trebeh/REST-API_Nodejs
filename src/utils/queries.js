eee9b3d72837983eeffa0adb9923595dd0a1df5e
Heberto Urribarri <60523990+0trebeh@users.noreply.github.com>
2021-03-02 14:08:56 -0400

    Update queries.js

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -11,7 +11,7 @@ module.exports = {
     getMenus: 'SELECT menu_id, title_menu FROM menu WHERE submenu IS NULL',
     getSub: 'WITH RECURSIVE ctemenu AS ( SELECT menu_id, title_menu, submenu FROM menu WHERE menu_id = $1 UNION ALL SELECT menu.menu_id, menu.title_menu, menu.submenu FROM menu JOIN ctemenu ON menu.submenu = ctemenu.menu_id) SELECT ctemenu.menu_id, ctemenu.title_menu, ctemenu.submenu, form.form_id, form.title_form FROM ctemenu LEFT JOIN form ON ctemenu.menu_id = form.menu_id ORDER BY ctemenu.menu_id',
     getQuestion: 'SELECT * FROM question INNER JOIN type_question on question.form_id = $1 AND question.question_id = type_question.question_id INNER JOIN form ON form.form_id = $1',
-    getAnswer: 'SELECT form.title_form, question.title_q, question.value, answer.value, answer.user_id FROM answer INNER JOIN question ON answer.question_id = question.question_id INNER JOIN form ON form.form_id = $1',
+    getAnswer: 'SELECT form.title_form, question.title_q, question.value AS question, answer.value, answer.user_id FROM answer INNER JOIN question ON answer.question_id = question.question_id INNER JOIN form ON form.form_id = $1',
     getAnswerUser: 'SELECT form.title_form FROM answer INNER JOIN question ON answer.question_id = question.question_id INNER JOIN form ON form.form_id = question.form_id AND answer.user_id = $1',
     createMenu: 'INSERT INTO menu (title_menu, user_id, submenu) VALUES ($1, $2, $3) RETURNING *',
     createForm: 'INSERT INTO form (menu_id, title_form, description_form, locked) VALUES ($1, $2, $3, $4) RETURNING *',

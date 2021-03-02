6cdb7135231646dca95802055d6122d4df436db5
Heberto <hebertourribarri2@gmail.com>
2021-03-01 23:17:28 -0400

    queri

--- src/controllers/forms.controllers.js
+++ src/controllers/forms.controllers.js
@@ -47,7 +47,7 @@ const getAnswer = async (req, res) => {
   const client = await pool.connect();
   const id = parseInt(req.params.id);
   try {
-    const response = await client.query('SELECT answer.value FROM answer INNER JOIN question ON answer.question_id = question.question_id INNER JOIN form ON form.form_id = $1', [
+    const response = await client.query('SELECT form.title_form, question.title_q, answer.value FROM answer INNER JOIN question ON answer.question_id = question.question_id INNER JOIN form ON form.form_id = $1', [
       id
     ]);
     res.status(200).json(response.rows);
